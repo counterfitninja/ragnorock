42adde61a624f68372aefd971cf5e9646c2afc13
counterfitninja <dave@chaoticblue.co.uk>
2024-03-11 17:08:32 +0000

    home

--- landmarks/ragnarok.js
+++ landmarks/ragnarok.js
@@ -27,6 +27,7 @@ var events = [
 var mark = [
         
         // tribe bases
+        [16.5, 23.6, 'home', 'cadetblue', 'Home Sweet Home'],
         [30.3, 32.7, 'home', 'darkblue', 'Crystal Cave'],
         [31.7, 34.2, 'home', 'darkblue', 'Oil Pumps'],
         [27.2, 42.2, 'home', 'darkblue', 'Beavers']
